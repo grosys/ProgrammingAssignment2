156cbd7bbbf93e50da8dfd7cd5f138a2fba8ec5a
Gary Rosys <rosys@bellsouth.net>
2015-04-26 17:10:50 -0400

    Rosys: second function needs comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions combine to cache the inv of a matrix.
+## The first creates a list of functions that inits the process
+## The second uses the list to check for and return the cached inverse
+##     If there's no cached inverse matrix it computes the inverse.
 
-## Write a short comment describing this function
+## This is the init step: creates list object to identify if an 
+##     inverse is cached and the cached matrix itself.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(oldMat = matrix()) {
+    invMat <- NULL                ## don't have inv yet.
+    set <- function(y) {          
+        oldMat <<- y              ## creates fcn lex scope calls oldMat 
+        invMat <<- NULL
+    }
+    get <- function() oldMat      
+    setInv <- function(solve) invMat <<- solve
+    getInv <- function() invMat   ## hopefully gives cached inverse
+    list(set = set, get = get,
+        setInv = setInv,
+        getInv = getInv)
 }
 
 
@@ -12,4 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invMat <- x$getInv()
+  if(!is.null(invMat)) {
+      message("getting cached data")
+      return(invMat)
+    }
+  data <- x$get()
+  invMat <- solve(data, ...)
+  x$setInv(invMat)
+  invMat
 }
